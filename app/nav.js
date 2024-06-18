ea8c71338e9687b807f795c08043dcee7719fa4b
Lars Desmet <volltixR200@gmail.com>
2024-06-19 01:10:09 +0200

    fixed some things with the sidenav

--- app/nav.js
+++ app/nav.js
@@ -3,30 +3,12 @@ import './nav.css';
 import jwt from 'jsonwebtoken';
 import { jwtConfig } from '../lib/jwt';
 import { Logout } from './logout/api_logout'; // Corrected import path and component name
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'; // Importing Font Awesome icons
 
 function Nav() {
     const [username, setUsername] = useState('');
-    const [isMobile, setIsMobile] = useState(false);
     const [isOpen, setIsOpen] = useState(false);
-
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            const handleResize = () => {
-                setIsMobile(window.innerWidth <= 768);
-            };
-
-            // Initial check
-            setIsMobile(window.innerWidth <= 768);
-
-            // Attach the event listener
-            window.addEventListener('resize', handleResize);
-
-            // Clean up the event listener
-            return () => {
-                window.removeEventListener('resize', handleResize);
-            };
-        }
-    }, []);
 
     useEffect(() => {
         function decodeToken(token) {
@@ -63,14 +45,14 @@ function Nav() {
     return (
         <header className="navbar">
             <div className="container">
-                <div className="profile" onClick={toggleMenu}>
+                <div onClick={toggleMenu} aria-label="Toggle navigation">
+                    <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
+                </div>
+                <div className="profile">
                     <h1>{username}</h1>
                 </div>
-                <div className="nav-toggle" onClick={toggleMenu} aria-label="Toggle navigation">
-                    <i className={`fas ${isOpen ? 'fa-times' : 'fa-bars'}`}></i>
-                </div>
             </div>
-            <nav className={`sidenav ${isOpen ? 'open' : ''}`}>
+            <nav className={`sidenav ${isOpen ? 'open' : 'closed'}`}>
                 <ul className="nav-links">
                     <li><a href="/dashboard">Dashboard</a></li>
                     <li><a href="/attitudekaarten">Attitude Kaarten</a></li>
